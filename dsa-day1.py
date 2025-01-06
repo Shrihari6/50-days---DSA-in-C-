7afe42c0cb4f163bcc94a00a70f92193b1d6a765
Shrihari J <163388353+Shrihari6@users.noreply.github.com>
2025-01-06 23:01:29 +0530

    dsa day:1 Jan 6 , Monday

--- dsa-day1.py
+++ dsa-day1.py
@@ -1,8 +1,7 @@
-
-### Day 1: Arrays
+"""Day 1: Arrays
 **Question**: Given an array of integers, return the indices of the two numbers that add up to a specific target.
 **Answer**:
-###
+"""
 def two_sum(nums, target):
     num_dict = {}
     for i, num in enumerate(nums):
